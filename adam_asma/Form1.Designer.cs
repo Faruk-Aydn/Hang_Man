097a9aa01c5641714fd18fe11475f37d9e4b7515
FARUK <farukaydin8245@gmail.com>
2024-12-11 10:34:09 +0300

    İlk proje yüklemesi

---
+++ adam_asma/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Hang_Man
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtHarf;
+        private System.Windows.Forms.Button btnKontrol;
+        private System.Windows.Forms.Label lblKelime;
+        private System.Windows.Forms.Label lblYanlisHarfler;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Label lblTitle; // Başlık etiketi
+        private System.Windows.Forms.Panel pnlTitle; // Başlık paneli
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            this.txtHarf = new System.Windows.Forms.TextBox();
+            this.btnKontrol = new System.Windows.Forms.Button();
+            this.lblKelime = new System.Windows.Forms.Label();
+            this.lblYanlisHarfler = new System.Windows.Forms.Label();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label(); // Başlık etiketi
+            this.pnlTitle = new System.Windows.Forms.Panel(); // Başlık paneli
+            this.SuspendLayout();
+
+            // 
+            // pnlTitle
+            // 
+            this.pnlTitle.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.pnlTitle.Controls.Add(this.lblTitle);
+            this.pnlTitle.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitle.Name = "pnlTitle";
+            this.pnlTitle.Size = new System.Drawing.Size(584, 50);
+            this.pnlTitle.TabIndex = 6;
+
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblTitle.Location = new System.Drawing.Point(200, 10);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(190, 37);
+            this.lblTitle.TabIndex = 5;
+            this.lblTitle.Text = "Hang Man";
+
+            // 
+            // txtHarf
+            // 
+            this.txtHarf.Location = new System.Drawing.Point(300, 150);
+            this.txtHarf.Name = "txtHarf";
+            this.txtHarf.Size = new System.Drawing.Size(50, 20);
+            this.txtHarf.TabIndex = 0;
+
+            // 
+            // btnKontrol
+            // 
+            this.btnKontrol.Location = new System.Drawing.Point(300, 200);
+            this.btnKontrol.Name = "btnKontrol";
+            this.btnKontrol.Size = new System.Drawing.Size(75, 23);
+            this.btnKontrol.TabIndex = 1;
+            this.btnKontrol.Text = "Kontrol Et";
+            this.btnKontrol.UseVisualStyleBackColor = true;
+            this.btnKontrol.BackColor = System.Drawing.Color.LightGray;
+            this.btnKontrol.Font = new System.Drawing.Font("Arial", 10, System.Drawing.FontStyle.Bold);
+            this.btnKontrol.Click += new System.EventHandler(this.BtnKontrol_Click);
+
+            // 
+            // lblKelime
+            // 
+            this.lblKelime.AutoSize = true;
+            this.lblKelime.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKelime.Location = new System.Drawing.Point(250, 100); // Oyun öğelerini biraz aşağıya çektik
+            this.lblKelime.Name = "lblKelime";
+            this.lblKelime.Size = new System.Drawing.Size(103, 36);
+            this.lblKelime.TabIndex = 2;
+            this.lblKelime.Text = "______";
+
+            // 
+            // lblYanlisHarfler
+            // 
+            this.lblYanlisHarfler.AutoSize = true;
+            this.lblYanlisHarfler.Location = new System.Drawing.Point(300, 250);
+            this.lblYanlisHarfler.Name = "lblYanlisHarfler";
+            this.lblYanlisHarfler.Size = new System.Drawing.Size(75, 13);
+            this.lblYanlisHarfler.TabIndex = 3;
+            this.lblYanlisHarfler.Text = "Yanlış Harfler: ";
+
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Location = new System.Drawing.Point(300, 280);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(47, 13);
+            this.lblSkor.TabIndex = 4;
+            this.lblSkor.Text = "Skor: 0";
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.pnlTitle);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.lblYanlisHarfler);
+            this.Controls.Add(this.lblKelime);
+            this.Controls.Add(this.btnKontrol);
+            this.Controls.Add(this.txtHarf);
+            this.Name = "Form1";
+            this.Text = "Adam Asmaca - Türkiye Şehirleri";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.pnlTitle.ResumeLayout(false);
+            this.pnlTitle.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+    }
+}
---
+++ adam_asma/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Hang_Man
+{
+    public partial class Form1 : Form
+    {
+        int hataSayisi = 0; // Çöp adamın aşamaları için hata sayacı
+        int skor = 0; // Skor sayacı
+        List<string> sehirler = new List<string> { "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Aksaray", "Amasya", "Ankara", "Antalya", "Ardahan", "Artvin", "Aydın", "Balıkesir", "Bartın", "Batman", "Bayburt", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Iğdır", "Isparta", "İstanbul", "İzmir", "Kahramanmaraş", "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri", "Kırıkkale", "Kırklareli", "Kırşehir", "Kilis", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Mardin", "Mersin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Şanlıurfa", "Şırnak", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak" };
+
+        string secilenSehir;
+        char[] gizliKelime;
+        List<char> kullanilanHarfler = new List<char>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            OyunBaslat();
+            this.BackColor = Color.LightBlue; // Arka plan rengini değiştir
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            OyunBaslat();
+        }
+
+        private void OyunBaslat()
+        {
+            Random rand = new Random();
+            secilenSehir = sehirler[rand.Next(sehirler.Count)].ToUpper();
+            gizliKelime = new string('_', secilenSehir.Length).ToCharArray();
+            hataSayisi = 0;
+            skor = 0; // Skoru sıfırla
+            kullanilanHarfler.Clear();
+            lblYanlisHarfler.Text = "Yanlış Harfler: ";
+            lblSkor.Text = $"Skor: {skor}";
+            GosterKelime();
+            this.Invalidate();
+        }
+
+        private void GosterKelime()
+        {
+            lblKelime.Text = string.Join(" ", gizliKelime);
+            lblKelime.Font = new Font("Arial", 24, FontStyle.Bold); // Fontu güncelle
+            lblKelime.ForeColor = Color.DarkBlue; // Yazı rengini değiştir
+        }
+
+        private void BtnKontrol_Click(object sender, EventArgs e)
+        {
+            string girilenHarf = txtHarf.Text.ToUpper();
+
+            if (girilenHarf.Length == 1 && !kullanilanHarfler.Contains(girilenHarf[0]))
+            {
+                kullanilanHarfler.Add(girilenHarf[0]);
+
+                if (secilenSehir.Contains(girilenHarf))
+                {
+                    for (int i = 0; i < secilenSehir.Length; i++)
+                    {
+                        if (secilenSehir[i] == girilenHarf[0])
+                        {
+                            gizliKelime[i] = girilenHarf[0];
+                        }
+                    }
+                    skor += 10; // Doğru tahmin için skor arttır
+                    GosterKelime();
+                }
+                else
+                {
+                    hataSayisi++;
+                    lblYanlisHarfler.Text += girilenHarf + " "; // Yanlış harfi göster
+                    this.Invalidate(); // Formu yeniden çiz (çöp adamı güncelle)
+                }
+
+                txtHarf.Clear();
+
+                if (!gizliKelime.Contains('_'))
+                {
+                    MessageBox.Show("Tebrikler! Şehri doğru tahmin ettiniz.");
+                    skor += 50; // Oyunu kazanma skoru
+                    OyunBaslat();
+                }
+                else if (hataSayisi == 7)
+                {
+                    MessageBox.Show($"Kaybettiniz! Doğru şehir: {secilenSehir}");
+                    OyunBaslat();
+                }
+                lblSkor.Text = $"Skor: {skor}";
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            Pen kalem = new Pen(Color.Black, 2);
+
+            // Çöp adamın direği, sadece hata sayısı 1 veya daha fazlaysa çizilecek
+            if (hataSayisi > 0)
+            {
+                g.DrawLine(kalem, 50, 50, 50, 250); // Dikey çizgi
+                g.DrawLine(kalem, 50, 50, 150, 50); // Yatay çizgi
+                g.DrawLine(kalem, 150, 50, 150, 100); // Kanca
+            }
+
+            // Çöp adamın her aşamasını sırayla çizeceğiz
+            if (hataSayisi > 1)
+            {
+                // Baş (çember)
+                g.DrawEllipse(kalem, 125, 100, 50, 50);
+            }
+            if (hataSayisi > 2)
+            {
+                // Gövde (dik çizgi)
+                g.DrawLine(kalem, 150, 150, 150, 250);
+            }
+            if (hataSayisi > 3)
+            {
+                // Sağ kol
+                g.DrawLine(kalem, 150, 180, 200, 130);
+            }
+            if (hataSayisi > 4)
+            {
+                // Sol kol
+                g.DrawLine(kalem, 150, 180, 100, 130);
+            }
+            if (hataSayisi > 5)
+            {
+                // Sağ bacak
+                g.DrawLine(kalem, 150, 250, 200, 300);
+            }
+            if (hataSayisi > 6)
+            {
+                // Sol bacak
+                g.DrawLine(kalem, 150, 250, 100, 300);
+            }
+        }
+    }
+}
